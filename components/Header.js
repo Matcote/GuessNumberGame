2a8144315da32a91aaeffaf8cb3b2c0fb5e22391
Matcote <matthewcotesipod@gmail.com>
2020-11-05 15:43:09 -0500

    header added

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = ({title}) =>{
+    return <Wrapper>
+        <Title>{title}</Title>
+    </Wrapper>
+}
+
+const Wrapper = styled.View`
+    width: 100%;
+    height: 90px;
+    padding-top: 36px;
+    background-color: #f7287b;
+    align-items: center;
+    justify-content: center;
+`;
+const Title = styled.Text`
+    font-size: 22px;
+    color: black;
+`;
+
+export default Header;
